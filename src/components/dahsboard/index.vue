b86452e75f43ad8a9fdfc0bf1d9851fb10404d7d
Ronan <ronan.ronandavid@gmail.com>
2021-02-19 12:37:58 +0100

    structure

---
+++ src/components/dahsboard/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="dashboard">
+        <!-- composant météo du jour -->
+        <!-- composant météo de la semaine : v-for de la réponse axios sur 7 jours-->
+    </section>
+</template>
+<script>
+export default {
+    name: 'DashBoard'
+}
+</script>
+<style lang="sass">
+
+</style>
---
+++ src/components/predictionBox/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="weather-box">
+        <!-- box de présentation de la météo à venir -->
+    </section>
+</template>
+<script>
+export default {
+    name: 'PredictionBox'
+}
+</script>
+<style lang="sass">
+
+</style>
