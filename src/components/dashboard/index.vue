fe5fa8b441666bc13938cd3d6d5ea7be19069e0f
Ronan <ronan.ronandavid@gmail.com>
2021-02-19 15:29:54 +0100

    structure weather box

--- src/components/dashboard/index.vue
+++ src/components/dashboard/index.vue
@@ -1,22 +1,40 @@
 <template>
     <section class="dashboard">
         <!-- composant météo du jour -->
+        <div class="dashboard__today-weather">
+            <weather-box
+                v-if="getIcon"
+                :cityName="cityName"
+                :description="description"
+                :humidity="humidity"
+                :temperature="temperature"
+                :getIcon='getIcon'
+            />
+        </div>
         <!-- composant météo de la semaine : v-for de la réponse axios sur 7 jours-->
     </section>
 </template>
 <script>
 
 import axios from 'axios'
+import WeatherBox from '../weatherBox'
 
 export default {
     name: 'DashBoard',
+    components: {
+        WeatherBox
+    },
     data () {
         return {
             dailyUrl: 'https://api.openweathermap.org/data/2.5/weather?q=',
             oneCall: 'https://api.openweathermap.org/data/2.5/onecall?',
             appKey: '&appid=139356f9973cdaecfb9ffff4f0ee7d31',
             lang:'&lang=fr',
-            cityName: ''
+            cityName: '',
+            description: '',
+            humidity: null,
+            temperature: null,
+            getIcon: null
         }
     },
     methods: {
@@ -31,10 +49,14 @@ export default {
             navigator.geolocation.getCurrentPosition(success, error)
         },
         async getCityName(lat, lon) {
-            const getCity = this.dailyUrl + lat + lon + this.appKey + this.lang
+            const getCity = this.dailyUrl + lat + lon + '&units=metric' + this.lang + this.appKey
             try {
                 const RESPONSE = await axios.get(getCity)
                 this.cityName = RESPONSE.data.name
+                this.description = RESPONSE.data.weather[0].description
+                this.humidity = RESPONSE.data.main.humidity
+                this.temperature = Math.floor(RESPONSE.data.main.temp)
+                this.getIcon = RESPONSE.data.weather[0].icon
             } catch (error) {
                 console.error('error getCityName: ', error)
             }
@@ -43,7 +65,7 @@ export default {
             const LAT = '&lat=' + crd.latitude
             const LON = '&lon=' + crd.longitude
             this.getCityName(LAT, LON)
-            const WEATHERURL = this.oneCall + LAT + LON + this.appKey + this.lang
+            const WEATHERURL = this.oneCall + LAT + LON + '&units=metric' +this.appKey + this.lang
             try {
                 await axios.get(WEATHERURL)
             } catch (error) {
@@ -56,6 +78,23 @@ export default {
     }
 }
 </script>
-<style lang="sass">
+<style lang="scss">
+@import '@/assets/style/mixins';
 
+.dashboard {
+    &__today-weather {
+        width: 100%;
+        border-radius: pxToRem(5);
+        border: 1px solid grey;
+        margin: 0 auto;
+
+        @include media-breakpoint-up(md) {
+            width: 50%
+        }
+
+        @include media-breakpoint-up(lg) {
+            width: 25%
+        }
+    }
+}
 </style>
---
+++ src/components/weatherBox/index.vue
@@ -0,0 +1,60 @@
+<template>
+    <section class="weather-box">
+        <!-- box de présentation de la météo à venir -->
+        <h2>{{ cityName }}</h2>
+        <h3>{{ description }}</h3>
+        <img :src="icon" />
+        <p>{{ temperature }}°</p>
+        <p>Humidité: : {{ humidity }} %</p>
+    </section>
+</template>
+<script>
+export default {
+    name: 'weatherBox',
+    data () {
+        return {
+        }
+    },
+    props: {
+        cityName: {
+            type: String,
+            default: () => (''),
+            required: false
+        },
+        description: {
+            type: String,
+            default: () => (''),
+            required: false
+        },
+        humidity: {
+            type: Number,
+            default: () => 0,
+            required: false
+        },
+        temperature: {
+            type: Number,
+            default: () => 0,
+            required: false
+        },
+        getIcon: {
+            type: String,
+            default: () => (''),
+            required: false
+        }
+    },
+    computed: {
+        icon () {
+            return `http://openweathermap.org/img/wn/${this.getIcon}@2x.png`
+        }
+    }
+}
+</script>
+<style lang="scss">
+
+.weather-box {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+}
+</style>
